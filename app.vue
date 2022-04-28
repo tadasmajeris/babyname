7acf693d86a838dd6a166079a5db196f07ed14d9
Spicy Bureau <spicybureau@B42-iMac-SpicyBureau.local>
2022-04-28 11:04:24 +0100

    basic setup

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>bbnm</h1>
+    <div class="options">
+      <div class="option">
+        <h4>1) Gender</h4>
+        <button>Boy</button>
+        <button>Unisex</button>
+        <button>Girl</button>
+      </div>
+      <div class="option">
+        <h4>2) Popularity</h4>
+        <button>Trendy</button>
+        <button>Unique</button>
+      </div>
+      <div class="option">
+        <h4>3) Length</h4>
+        <button>All</button>
+        <button>Long</button>
+        <button>Short</button>
+      </div>
+    </div>
+  </div>
+</template>
